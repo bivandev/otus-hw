b4ac9164c12d0656d43cdf241b1f0e7654b91a78
Басенко Иван <ivbasenko@cloud.ru>
2024-11-22 01:06:24 +0300

    hw04 is complete

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,9 @@
 package hw04lrucache
 
+import (
+	"fmt"
+)
+
 type Key string
 
 type Cache interface {
@@ -9,17 +13,69 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
-	items    map[Key]*ListItem
+	items    map[Key]*cacheItem
+}
+
+type cacheItem struct {
+	key   Key
+	value interface{}
+	item  *ListItem
+}
+
+func (l *lruCache) Set(key Key, value interface{}) bool {
+	item, ok := l.items[key]
+	if ok {
+		item.value = value
+
+		l.queue.MoveToFront(item.item)
+	} else {
+		if l.capacity == l.queue.Len() {
+
+			back := l.queue.Back()
+
+			valKey, ok := back.Value.(Key)
+			if !ok {
+				fmt.Println("Error while deleting cache item")
+				return ok
+			}
+
+			delete(l.items, valKey)
+
+			l.queue.Remove(back)
+		}
+
+		l.items[key] = &cacheItem{
+			key:   key,
+			value: value,
+			item:  l.queue.PushFront(key),
+		}
+	}
+
+	return ok
+}
+
+func (l *lruCache) Get(key Key) (interface{}, bool) {
+	item, ok := l.items[key]
+	if !ok {
+		return nil, false
+	}
+
+	l.queue.MoveToFront(item.item)
+
+	return item.value, true
+}
+
+func (l *lruCache) Clear() {
+	l.queue = new(list)
+	l.items = make(map[Key]*cacheItem, l.capacity)
 }
 
 func NewCache(capacity int) Cache {
 	return &lruCache{
 		capacity: capacity,
 		queue:    NewList(),
-		items:    make(map[Key]*ListItem, capacity),
+		items:    make(map[Key]*cacheItem, capacity),
 	}
 }
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "fmt"
+
 type List interface {
 	Len() int
 	Front() *ListItem
@@ -17,8 +19,104 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	len   int
+	front *ListItem
+	back  *ListItem
+}
+
+func (l list) Len() int {
+	return l.len
+}
+
+func (l list) Front() *ListItem {
+	return l.front
+}
+
+func (l list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	var item = new(ListItem)
+	item.Value = v
+	item.Next = l.front
+
+	if l.len == 0 {
+		l.back = item
+	} else {
+		l.front.Prev = item
+	}
+
+	l.front = item
+	l.len = l.len + 1
+
+	return item
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	var item = new(ListItem)
+	item.Value = v
+
+	if l.len == 0 {
+		l.front = item
+	} else {
+		l.back.Next = item
+	}
+
+	item.Prev = l.back
+	l.back = item
+	l.len = l.len + 1
+
+	return item
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i == nil || (i.Prev == nil && i.Next == nil && i != l.front) {
+		fmt.Println("Error while deleting cache item")
+		return
+	}
+
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	} else {
+		l.front = i.Next
+	}
+
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		l.back = i.Prev
+	}
+
+	i.Next = nil
+	i.Prev = nil
+	l.len = l.len - 1
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	exNext := i.Next
+	exPrev := i.Prev
+
+	if exPrev == nil {
+		return
+	}
+
+	if exNext == nil {
+		exPrev.Next = nil
+		l.back = exPrev
+	} else {
+		exPrev.Next = i.Next
+		exNext.Prev = i.Prev
+	}
+
+	exFront := l.Front()
+	exFront.Prev = i
+
+	i.Next = exFront
+
+	i.Prev = nil
+
+	l.front = i
 }
 
 func NewList() List {
