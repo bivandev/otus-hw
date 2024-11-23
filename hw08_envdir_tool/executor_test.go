63efc10787cab86d5619bc3b6c3736986fbe3856
Басенко Иван <ivbasenko@cloud.ru>
2024-11-23 16:56:43 +0300

    HW08 is complete

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,54 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("successfully run command with environment", func(t *testing.T) {
+		env := Environment{
+			"TEST_ENV_VAR": {Value: "test_value", NeedRemove: false},
+		}
+		cmd := []string{"env"}
+
+		exitCode, err := RunCmd(cmd, env)
+		require.NoError(t, err)
+		require.Equal(t, 0, exitCode)
+	})
+
+	t.Run("command not provided", func(t *testing.T) {
+		env := Environment{}
+		cmd := []string{}
+
+		exitCode, err := RunCmd(cmd, env)
+		require.Error(t, err)
+		require.Equal(t, 1, exitCode)
+	})
+
+	t.Run("remove environment variable", func(t *testing.T) {
+		os.Setenv("REMOVE_ME", "should_be_removed")
+		env := Environment{
+			"REMOVE_ME": {Value: "", NeedRemove: true},
+		}
+		cmd := []string{"env"}
+
+		exitCode, err := RunCmd(cmd, env)
+		require.NoError(t, err)
+		require.Equal(t, 0, exitCode)
+
+		_, exists := os.LookupEnv("REMOVE_ME")
+		require.False(t, exists)
+	})
+
+	t.Run("command fails with error", func(t *testing.T) {
+		env := Environment{}
+		cmd := []string{"false"}
+
+		exitCode, err := RunCmd(cmd, env)
+		require.NoError(t, err)
+		require.Equal(t, 1, exitCode)
+	})
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		fmt.Println("usage: go-envdir <envdir> <command> [<args>...]")
+		os.Exit(1)
+	}
+
+	envDir := os.Args[1]
+	cmd := os.Args[2:]
+
+	env, err := ReadDir(envDir)
+	if err != nil {
+		fmt.Printf("error reading envdir: %v\n", err)
+		os.Exit(1)
+	}
+
+	exitCode, err := RunCmd(cmd, env)
+	if err != nil {
+		fmt.Printf("error running command: %v\n", err)
+		os.Exit(1)
+	}
+
+	os.Exit(exitCode)
 }
